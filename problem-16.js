f99bdf67f36c9b3fa2ec982ac7019599feb43bca
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-05-01 15:06:27 -0700

    Problem 16

---
+++ problem-16.js
@@ -0,0 +1,48 @@
+// Power digit sum
+// 2 ** 15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+// What is the sum of the digits of the number 2 ** 1000?
+
+const multiplyArr = (arr, amt) => {
+  const carriedAmts = []
+  const multiplied = arr.map((el, i) => {
+    const groupLimit = 1e12
+    const product = el * amt
+
+    if (product > (groupLimit - 1)) {
+      // Carry the 1 over into the next group of millions
+      carriedAmts.push((i + 1))
+      return product - groupLimit
+    }
+
+    return product
+  })
+
+  // Loop back through carried millions and add them to respective groups
+  carriedAmts.forEach(index => {
+    multiplied[index] = multiplied[index] + 1 || 1
+  })
+  return multiplied
+}
+
+const largeExponent = (base, pow) => {
+  let products = [1]
+
+  for (let i = 0; i < pow; i++) {
+    products = multiplyArr(products, base)
+  }
+
+  return products
+}
+
+const sumOfDigits = exponentArr => {
+  const sum = exponentArr.map(digitGroup => {
+    const digits = digitGroup.toString().split('')
+    return digits.reduce((acc, digit) => parseInt(acc) + parseInt(digit))
+  })
+
+  return sum.reduce((acc, partialSum) => acc + partialSum)
+}
+
+// ✔ Solved: 1366
+console.log(sumOfDigits(largeExponent(2, 1000)))
+
