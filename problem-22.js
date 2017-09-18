b391185f9bfde21490f6950dc05f53144a81f7dc
Benjamin Johnson <benjamin.johnson@autogravity.com>
2017-09-17 21:18:17 -0700

    Problem 22

--- problem-22.js
+++ problem-22.js
@@ -1,14 +1,5 @@
 // Problem 22 -- Name Scores
 // https://projecteuler.net/problem=22
-
-/**
- * Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
- *
- * For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
- *
- * What is the total of all the name scores in the file?
- */
-
 const fs = require('fs')
 const path = require('path')
 const { isArray } = Array
@@ -30,5 +21,6 @@ function transformNameToValue (name, i) {
   return nameValue * (i + 1)
 }
 
+// ✔ Solved: 871198282
 console.log(transformNames(NAMES))
 
